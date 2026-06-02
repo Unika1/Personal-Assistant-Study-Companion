51d119f5e877e156383c81d61691073de99bf51c
Unika1 <unikaprj@gmail.com>
2026-06-02 23:03:26 +0545

    chatroute updated

--- src/routes/chatRoutes.js
+++ src/routes/chatRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { sendMessage } = require('../controllers/chatController');
+const authMiddleware = require('../middleware/authMiddleware');
 
 // Create a router for chat requests.
 const router = express.Router();
 
 // This route receives a student message and sends it to PASC.
-router.post('/', sendMessage);
+router.post('/', authMiddleware, sendMessage);
 
 module.exports = router;
